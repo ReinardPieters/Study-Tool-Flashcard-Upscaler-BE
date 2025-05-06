e02fe246afb913854d973c85957625a05103b710
JuanroRetro <156077669+Juanro-Minne@users.noreply.github.com>
2025-05-06 19:14:13 +0200

    added swagger

--- StudyToolFlashcardUpscaler/Program.cs
+++ StudyToolFlashcardUpscaler/Program.cs
@@ -1,12 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseSwagger();
 
-// Configure the HTTP request pipeline.
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; 
+});
 
 app.UseHttpsRedirection();
 
